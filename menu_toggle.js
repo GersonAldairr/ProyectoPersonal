d284df46b1494d7a2ba82c5b46f6a4768f096c56
Aldair C: Alburqueque <alburqueque544@gmail.com>
2025-11-27 17:01:32 +0000

    Listo

--- menu_toggle.js
+++ menu_toggle.js
@@ -2,7 +2,7 @@
 const btn = document.querySelector(".menu-toggle");
 
 // Selecciona el menú horizontal
-const menu = document.querySelector(".Menu-Horizontal");
+const menu = document.querySelector(".menu-horizontal");
 
 // Al hacer clic en el botón de hamburguesa
 btn.addEventListener("click", () => {
